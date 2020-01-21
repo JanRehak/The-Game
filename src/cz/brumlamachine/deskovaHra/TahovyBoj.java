d11cecc4d69d5fa8f03f67e4b640fe3471ad2514
Jan Řehák <jan.rehak33@gmail.com>
2020-01-21 19:02:07 +0100

    Added DEBUG mode

--- src/cz/brumlamachine/deskovaHra/TahovyBoj.java
+++ src/cz/brumlamachine/deskovaHra/TahovyBoj.java
@@ -6,26 +6,50 @@ public class TahovyBoj {
     public static void main(String[] args) {
 
         Kostka kostka = new Kostka(10);
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Vitej v arene!");
-//        System.out.println("Jmeno Tveho Bojovnika?");
-//        String jmeno1 = scanner.nextLine();
-//        System.out.println("Hodnota tveho utoku? Vyber si v rozmezi 0 - 40.");
-//        int utok1 = scanner.nextInt();
-//        int obrana1 = 40 - utok1;
-//        System.out.println("Hodnota tve obrany je: " + obrana1);
-//        System.out.println("Jmeno Tveho soupere?");
-//        String jmeno2 = scanner.next();
-//        System.out.println("Hodnota tveho utoku? Vyber si v rozmezi 0 - 40.");
-//
-//        int utok2 = scanner.nextInt();
-//        int obrana2 = 40 - utok2;
-//        System.out.println("Hodnota tve obrany je: " + obrana2);
 
-        Bojovnik Ilidanis = new Bojovnik("Ilidanis",100, 25, 20, kostka);
-        Bojovnik Gandalf = new Mag("Gandalf the White", 100, 10, 15, kostka, 10, 100, 150);
+        boolean debug = false;
+        int maxPoints = 40;
+        String jmeno1;
+        int utok1;
+        String jmeno2;
+        int utok2;
+        int obrana1;  //= maxPoints - utok1;
+        int obrana2; // = maxPoints - utok2;
+
+        if (!debug) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Vitej v arene!");
+            System.out.println("Jmeno Tveho Bojovnika?");
+            jmeno1 = scanner.nextLine();
+            System.out.println("Hodnota tveho utoku? Vyber si v rozmezi 0 - 40.");
+            utok1 = scanner.nextInt();
+            obrana1 = maxPoints - utok1;
+            System.out.println("Hodnota tve obrany je: " + obrana1);
+            System.out.println("Jmeno Tveho soupere?");
+            jmeno2 = scanner.next();
+            System.out.println("Hodnota tveho utoku? Vyber si v rozmezi 0 - 40.");
+
+            utok2 = scanner.nextInt();
+            obrana2 = maxPoints - utok2;
+            System.out.println("Hodnota tve obrany je: " + obrana2);
+
+        } else {
+
+            jmeno1 = "A";
+            utok1 = 40;
+            jmeno2 = "B";
+            utok2 = 20;
+            obrana1 = maxPoints - utok1;
+            obrana2 = maxPoints - utok2;
+        }
+
+
+        Bojovnik Ilidanis = new Bojovnik(jmeno1, 100, utok1, obrana1, kostka);
+        Bojovnik Gandalf = new Bojovnik(jmeno2, 100, utok2,obrana2, kostka);
         Arena arena = new Arena(Ilidanis, Gandalf, kostka);
+
+
 
         arena.zapas();
 
